8cc70061c994dd9d6001c289ea1bbe692980c02b
maynor.teleguario <uk004992@student.uni-kassel.de>
2023-09-06 19:51:10 +0200

    add cards into PortfolioBlock

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -2,7 +2,14 @@ import React from 'react';
 import IconLink from "./IconLink";
 import {Box} from "@mui/material";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container} from 'react-bootstrap'
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
+import Typography from '@mui/material/Typography';
+
 
 
 function PortfolioBlock(props) {
@@ -10,34 +17,43 @@ function PortfolioBlock(props) {
     /** image kann man hier setzen, wenn man es wieder braucht.*/
     const {videoLink, source, title, image, href} = props;
     return (
+        <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
+            <Card sx={{ maxWidth: 345, justifyContent: 'center'}}  >
+                <CardMedia sx={{ height: 140 }}>
+                    <div className="ratio ratio-21x9">
+                        <iframe
+                            src={videoLink}
+                            title="YouTube video"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+                </CardMedia>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
 
-        <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}
-             marginTop={'5rem'}>
-            <Container>
-                <div className="ratio ratio-21x9">
-                    <iframe
-                        src={videoLink}
-                        title="YouTube video"
-                        allowFullScreen
-                    ></iframe>
-                </div>
-            </Container>
-            <Box>
-                <a href={href} target="_blank" rel="noreferrer">
-                    <img src={image} alt="unity" width="80" height="80"/>
-                </a>
-            </Box>
+                        <a href={href} target="_blank" rel="noreferrer">
+                            <img src={image} alt="unity" width="80" height="80"/>
+                        </a>
 
-            <h1 style={{fontSize: '2rem'}}>{title}</h1>
-            <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
-                 alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        <h1 style={{fontSize: '2rem'}}>{title}</h1>
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
+                         alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
 
-                <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-                    <IconLink link={source} title={'Source Code'} icon={'fa fa-code'}/>
-                </Box>
-            </Box>
-
+                        <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
+                            <IconLink link={source} title={'Source Code'} icon={'fa fa-code'}/>
+                        </Box>
+                    </Box>
+                </CardActions>
+            </Card>
         </Box>
+
     );
 }
 
